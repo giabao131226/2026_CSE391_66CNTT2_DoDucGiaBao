f4d17a2e6dadcfa50dd76c8bd5a1ba0d40bc626f
giabao131226 <dobao13122006@gmail.com>
2026-03-16 00:00:05 +0700

    Done BTH4

--- BaiThucHanh4/Bai2/assets/js/main.js
+++ BaiThucHanh4/Bai2/assets/js/main.js
@@ -3,10 +3,10 @@
 function checkName() {
     const inputName = document.querySelector("form input[name='hovaten']")
     const value = inputName.value
-    if (value.trim().length < 3 || !/^[a-zA-ZÀ-ỹ\s]+$/.test(value)){
+    if (value.trim().length < 3 || !/^[a-zA-ZÀ-ỹ\s]+$/.test(value)) {
         inputName.classList.add("alert-error");
         return false;
-    } 
+    }
     return true;
 }
 // 
@@ -14,7 +14,7 @@ function checkName() {
 function checkEmail() {
     const inputEmail = document.querySelector("form input[name='email']")
     const value = inputEmail.value;
-    if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)){
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
         inputEmail.classList.add("alert-error")
         return false;
     }
@@ -25,7 +25,7 @@ function checkEmail() {
 function checkPhone() {
     const inputPhone = document.querySelector("form input[name='phone']")
     const value = inputPhone.value
-    if (value.length < 10 || value[0] != 0){
+    if (value.length < 10 || value[0] != 0) {
         inputPhone.classList.add("alert-error")
         return false;
     }
@@ -36,7 +36,7 @@ function checkPhone() {
 function checkPassword() {
     const inputPassword = document.querySelector("form input[name='password']")
     const value = inputPassword.value;
-    if (value.length < 8){
+    if (value.length < 8) {
         inputPassword.classList.add("alert-error");
         return false;
     }
@@ -47,19 +47,19 @@ function checkPassword() {
         else if (value[i] == value[i].toLowerCase()) haveThuong = true;
         else if (value[i] == value[i].toUpperCase()) haveHoa = true;
     }
-    if(haveHoa==false || haveThuong==false || haveSo==false){
+    if (haveHoa == false || haveThuong == false || haveSo == false) {
         inputPassword.classList.add("alert-error");
         return false;
     }
     return true;
 }
 // Hàm check khớp mật khẩu
-function checkKhopMatKhau(){
+function checkKhopMatKhau() {
     const inputXacNhanMatKhau = document.querySelector("form input[name='xacNhanMatKhau']")
     const inputPassword = document.querySelector("form input[name='password']")
     const value = inputXacNhanMatKhau.value;
     const passWord = inputPassword.value
-    if(value!=passWord){
+    if (value != passWord) {
         const inputXacNhanMatKhau = document.querySelector("form input[name='xacNhanMatKhau']")
         inputXacNhanMatKhau.classList.add("alert-error");
         return false;
@@ -79,32 +79,32 @@ function handleSubmit(e) {
         alert("Thông Tin Bạn Nhập Vào Có Lỗi. Vui Lòng Kiểm Tra Lại")
         return;
     }
-    if (!checkEmail()){
+    if (!checkEmail()) {
         alert("Thông Tin Bạn Nhập Vào Có Lỗi. Vui Lòng Kiểm Tra Lại")
         console.log("lỗi email")
         return;
     }
-    if(!checkPhone()){
+    if (!checkPhone()) {
         alert("Thông Tin Bạn Nhập Vào Có Lỗi. Vui Lòng Kiểm Tra Lại")
         console.log("lỗi phone")
         return;
     }
-    if(!checkPassword()){
+    if (!checkPassword()) {
         alert("Thông Tin Bạn Nhập Vào Có Lỗi. Vui Lòng Kiểm Tra Lại")
         console.log("lỗi mật khẩu")
         return;
     }
-    if(!checkKhopMatKhau()){
+    if (!checkKhopMatKhau()) {
         alert("Thông Tin Bạn Nhập Vào Có Lỗi. Vui Lòng Kiểm Tra Lại")
         console.log("Lỗi không khớp mật khẩu")
         return
     }
-    if(!inputGioiTinh){
+    if (!inputGioiTinh) {
         alert("Thông Tin Bạn Nhập Vào Có Lỗi. Vui Lòng Kiểm Tra Lại")
         console.log("Lỗi")
         return;
     }
-    if(!inputCheckBox){
+    if (!inputCheckBox) {
         alert("Thông Tin Bạn Nhập Vào Có Lỗi. Vui Lòng Kiểm Tra Lại")
         console.log("Lỗi CheckBox")
         return;
@@ -125,7 +125,7 @@ function handleSubmit(e) {
         <p>Email: <span class = "font-bold">${inputEmail.value}</span></p>
         <p>Phone: <span class = "font-bold">${inputPhone.value}</span></p>
     `
-} 
+}
 
 const form = document.querySelector("form")
 if (form) {
@@ -134,61 +134,111 @@ if (form) {
 // 
 
 // Thêm att checked cho input radio
-function handleClickRadio(e){
+function handleClickRadio(e) {
 
-    e.target.setAttribute("checked",true);
+    e.target.setAttribute("checked", true);
     const value = e.target.value;
-    if(value=="nam"){
+    if (value == "nam") {
         const anotherInputGioiTinh = document.querySelector("form input[value='nu']");
         anotherInputGioiTinh.removeAttribute("checked")
-    }else{
+    } else {
         const anotherInputGioiTinh = document.querySelector("form input[value='nam']");
         anotherInputGioiTinh.removeAttribute("checked")
     }
 }
 const inputGioiTinh = document.querySelectorAll("form input[name='gioiTinh']")
 inputGioiTinh.forEach((item) => {
-    item.addEventListener("click",handleClickRadio)
+    item.addEventListener("click", handleClickRadio)
 })
 // 
 // Hàm thêm checked vào inputCheckBox
-const inputCheckBox =document.querySelector("form input[type = 'checkbox']")
-if(inputCheckBox){
-    inputCheckBox.addEventListener("click",(e) => {
+const inputCheckBox = document.querySelector("form input[type = 'checkbox']")
+if (inputCheckBox) {
+    inputCheckBox.addEventListener("click", (e) => {
         const value = e.target.getAttribute("checked")
-        if(value){
+        if (value) {
             e.target.removeAttribute("checked")
             return;
         }
-        e.target.setAttribute("checked",true)
+        e.target.setAttribute("checked", true)
     })
 }
 // 
 
 // Validate realtime And
-function deleteAlertError(e){
+function deleteAlertError(e) {
     e.target.classList.remove("alert-error")
 }
 
+function checkManhYeu() {
+    const inputPassword = document.querySelector("form input[name='password']")
+    const value = inputPassword.value;
+    if (value.length < 8) {
+        inputPassword.classList.add("alert-error");
+    }
+
+    let haveHoa = false, haveThuong = false, haveSo = false;
+    for (let i = 0; i < value.length; i++) {
+        if (!isNaN(value[i])) haveSo = true;
+        else if (value[i] == value[i].toLowerCase()) haveThuong = true;
+        else if (value[i] == value[i].toUpperCase()) haveHoa = true;
+    }
+    if (haveHoa == true || haveThuong == true) {
+        inputPassword.classList.remove("alert-error")
+        inputPassword.classList.add("border-yellow")
+    }
+    if (haveHoa == true && haveThuong == true && haveSo == true) {
+        inputPassword.classList.remove("border-yellow")
+        inputPassword.classList.add("border-green")
+    }
+}
+
 const inputName = document.querySelector("form input[name='hovaten']")
-inputName.addEventListener("blur",checkName)
-inputName.addEventListener("keyup",deleteAlertError)
+inputName.addEventListener("blur", checkName)
+inputName.addEventListener("keyup", deleteAlertError)
 
 const inputEmail = document.querySelector("form input[name='email']")
-inputEmail.addEventListener("blur",checkEmail)
-inputEmail.addEventListener("keyup",deleteAlertError)
+inputEmail.addEventListener("blur", checkEmail)
+inputEmail.addEventListener("keyup", deleteAlertError)
 
 const inputPhone = document.querySelector("form input[name='phone']")
-inputPhone.addEventListener("blur",checkPhone)
-inputPhone.addEventListener("keyup",deleteAlertError)
+inputPhone.addEventListener("blur", checkPhone)
+inputPhone.addEventListener("keyup", deleteAlertError)
 
 const inputPassword = document.querySelector("form input[name='password']")
-inputPassword.addEventListener("blur",checkPassword)
-inputPassword.addEventListener("keyup",deleteAlertError)
+inputPassword.addEventListener("blur", checkPassword)
+inputPassword.addEventListener("keyup", deleteAlertError)
+inputPassword.addEventListener("keyup", checkManhYeu)
 
 const inputXacNhanMatKhau = document.querySelector("form input[name='xacNhanMatKhau']")
-inputXacNhanMatKhau.addEventListener("blur",checkKhopMatKhau)
-inputXacNhanMatKhau.addEventListener("keyup",deleteAlertError)
+inputXacNhanMatKhau.addEventListener("blur", checkKhopMatKhau)
+inputXacNhanMatKhau.addEventListener("keyup", deleteAlertError)
 // 
 
-
+const listHienThi = document.querySelectorAll("button.hienThiPassword")
+console.log(listHienThi)
+listHienThi.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        const element = e.target.getAttribute("id")
+        const tt = e.target.getAttribute("tt")
+        if (element == "pass") {
+            if (tt == "hidden") {
+                e.target.setAttribute("tt", "show")
+                inputPassword.setAttribute("type", "text")
+            } else {
+                e.target.setAttribute("tt", "hidden")
+                inputPassword.setAttribute("type", "password")
+            }
+        } else {
+            if (tt == "hidden") {
+                e.target.setAttribute("tt", "show")
+                const inputXacNhanMatKhau = document.querySelector("form input[name='xacNhanMatKhau']")
+                inputXacNhanMatKhau.setAttribute("type", "text")
+            } else {
+                e.target.setAttribute("tt", "hidden")
+                const inputXacNhanMatKhau = document.querySelector("form input[name='xacNhanMatKhau']")
+                inputXacNhanMatKhau.setAttribute("type", "password")
+            }
+        }
+    })
+})
